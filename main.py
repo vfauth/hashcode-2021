8cd17b9c4bf6b7d2c6857ef8e2feb84914b02193
paultrh <paultr_h@epita.fr>
2021-02-25 19:35:27 +0100

    work

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #!/bin/python3
+import numpy as np
 
 def main():
-    print(parse_file("inputs/a.txt"))
-    duration, nb_intersec, streets, cars = parse_file("inputs/a.txt")
+    #print(parse_file("inputs/a.txt"))
+
+    duration, nb_intersec, adj, streets, cars = parse_file("inputs/b.txt")
+    print(streets)
+
+    path_duration = []
+
+    for index, car in enumerate(cars):
+        print(car)
+        sum_street = 0
+        for street in car[1:]:
+            sum_street += streets[street][2]
+
+        path_duration.append(sum_street)
+
+    print(max(path_duration))
+    print(np.mean([len(i) for i in cars]))
+
 
 
 def parse_file(path):
@@ -17,20 +34,28 @@ def parse_file(path):
 
         # Streets
         streets = dict()
+
+        adj = dict()
         for i in range(nb_streets):
             street = f.readline().split(" ")
             origin = int(street[0])
             dest = int(street[1])
-            if origin not in streets:
-                streets[origin] = dict()
-            streets[origin][dest] = (street[2], int(street[3]))
+            if origin not in adj:
+                adj[origin] = dict()
+            adj[origin][dest] = (street[2], int(street[3]))
+
+            streets[street[2]] = (origin, dest, int(street[3]))
 
         # Cars
         cars = []
         for i in range(nb_cars):
-            car = f.readline().split(" ")
+            car = f.readline().replace('\n', '').split(" ")
             cars.append(car[1:])
 
-        return duration, nb_intersec, streets, cars
+
+
+        return duration, nb_intersec, adj, streets, cars
+
+
 
 main()
